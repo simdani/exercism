0e5847203f501be7fdb4c8c1a2092f25f6805dc2
Simonas Daniliauskas <dranxman@gmail.com>
2019-01-03 18:48:27 +0200

    finish word count solution

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,6 +1,6 @@
 class Words {  
     count(word: string): Map<String, number> {
-        let words: String[] = word.split(" ").map(word => word.toLowerCase());
+        let words: String[] = word.trim().split(/\s+/).map((word) => word.toLowerCase());
         let wordCount:Map<String, number> = new Map();
         for (let i = 0; i < words.length; i++) {
             if (!wordCount.has(words[i])) {
